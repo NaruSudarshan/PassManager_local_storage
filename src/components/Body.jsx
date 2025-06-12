9291de6a87701557a9308797299f808e0cd8b093
NaruSudarshan <narusudarshanreddy09@gmail.com>
2025-06-12 23:48:15 +0530

    ui changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import React, { useEffect ,useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
